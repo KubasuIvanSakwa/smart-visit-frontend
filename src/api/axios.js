0acdb75ef64d0ee894794868aff9ab9f45302c13
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2025-09-01 01:31:18 +0300

    return of api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-//   baseURL: 'http://127.0.0.1:8000  ', // No trailing slash
-  baseURL: 'http://127.0.0.1:8000/', // No trailing slash
+  baseURL: 'https://smart-visit-backend.onrender.com/', // Production backend URL
 });
 
 // Request Interceptor: Inject Bearer token if available
@@ -31,7 +30,7 @@ api.interceptors.response.use(
         const refreshToken = localStorage.getItem('refresh_token');
         if (refreshToken) {
           const response = await axios.post(
-            'http://127.0.0.1:8000/api/token/refresh/',
+            'https://smart-visit-backend.onrender.com/api/token/refresh/',
             { refresh: refreshToken }
           );
 
